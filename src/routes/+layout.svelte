1d3c9f8f651326363cb0880c726af6eb9e0880e7
sinYa Iwasaki <iwasaki@sinproject.net>
2023-09-27 15:15:47 +0900

    Install Tailwind CSS with SvelteKit #29

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css'
+</script>
+
+<slot />
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
